1e2e8c20afb107b83be522e85cb6374c1aca6837
Rodolpho Almeida <rodolpho_almeida@hotmail.com>
2022-06-21 13:46:35 -0300

    ajuste cookie

--- src/app/services/congregation.service.ts
+++ src/app/services/congregation.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from './../../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { userInfo } from 'os';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
